82f71510cc85dd727a55cb59ab4928fd2d3867b3
adyeardley <adyeardley@googlemail.com>
2018-12-04 14:25:27 +0000

    #001-UpdatedMainPage
    Added in routing for other pages in set up for framework.

--- MasterDebaters-App/src/app/app.module.ts
+++ MasterDebaters-App/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './routes/contact/contact.component';
+import { HomeComponent } from './routes/home/home.component';
+import { AboutComponent } from './routes/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
